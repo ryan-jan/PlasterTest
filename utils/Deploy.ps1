c10396f59314847bea1717aa0c4cb995a5bdbee4
Ryan Kowalewski <ryan@ryanjan.uk>
2020-05-29 12:41:32 +0100

    Update

--- utils/Deploy.ps1
+++ utils/Deploy.ps1
@@ -18,4 +18,13 @@ if ($CurModuleVersion -gt $PrevModuleVersion) {
 Start-Sleep -Seconds 5
 Rename-Item -Path ".\src" -NewName "PlasterTest" -Force -Confirm:$false
 $CurModuleVersion = (Import-PowerShellDataFile ".\PlasterTest\PlasterTest.psd1").ModuleVersion
-$Tag = (git tag --points-at master)[0]
+$Tag = @((git tag --points-at master))[0]
+
+if ($null -ne $Tag) {
+    if ($Tag -like "v$($CurModuleVersion.ToString)") {
+        Write-Host "Current commit has been tagged with a new version. Pushing to PowerShell Gallery."
+        #Publish-Module -Path "$PSScriptRoot\..\PlasterTest" -NuGetApiKey $env:PSGALLERY_KEY
+    } else {
+        throw "Current commit has been tagged with a new version, but the version differs from that specified in the module manifest."
+    }
+}
