0c1e64e124d90e15017ca5ac877cfaacf237cf2a
Ryan Kowalewski <ryan@ryanjan.uk>
2020-06-26 16:56:07 +0100

    update

--- utils/Deploy.ps1
+++ utils/Deploy.ps1
@@ -1,8 +1,10 @@
 if ($env:APPVEYOR_REPO_TAG -eq "true") {
     Rename-Item -Path ".\src" -NewName "PlasterTest" -Force -Confirm:$false
     $CurModuleVersion = (Import-PowerShellDataFile ".\PlasterTest\PlasterTest.psd1").ModuleVersion
-    
-    if ($env:APPVEYOR_REPO_TAG_NAME -like "v$($CurModuleVersion.ToString)") {
+    $env:APPVEYOR_REPO_TAG_NAME
+    $CurModuleVersion.ToString()
+
+    if ($env:APPVEYOR_REPO_TAG_NAME -like "v$($CurModuleVersion.ToString())") {
         Write-Host "Current commit has been tagged with a new version. Pushing to PowerShell Gallery."
         #Publish-Module -Path "$PSScriptRoot\..\PlasterTest" -NuGetApiKey $env:PSGALLERY_KEY
     } else {
