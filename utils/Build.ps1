4774868e930707a8aaea2f2fd208071a1f3509d1
Ryan Kowalewski <ryan@ryanjan.uk>
2020-06-26 17:02:45 +0100

    update

--- utils/Build.ps1
+++ utils/Build.ps1
@@ -5,8 +5,8 @@ param (
 )
 
 Write-Host "Running on PowerShell $($PSVersionTable.PSEdition) $($PSVersionTable.PSVersion.ToString())"
-$RepoRoot = Split-Path -Path $PSScriptRoot -Parent
-$ModulePath = Join-Path -Path $RepoRoot -ChildPath "src" -AdditionalChildPath "PlasterTest.psd1"
+$SrcPath = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath "src"
+$ModulePath = Join-Path -Path $SrcPath -ChildPath "PlasterTest.psd1"
 $TestsPath = Join-Path -Path $PSScriptRoot -ChildPath "Invoke-Tests.ps1"
 $DeployPath = Join-Path -Path $PSScriptRoot -ChildPath "Deploy.ps1"
 Import-Module $ModulePath
