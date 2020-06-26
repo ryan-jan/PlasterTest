988bebb792c1e8e350caa0c05fb23d7716606923
Ryan Kowalewski <ryan@ryanjan.uk>
2020-06-26 16:50:13 +0100

    update

--- utils/Deploy.ps1
+++ utils/Deploy.ps1
@@ -1,27 +1,8 @@
-<#
-Start-Sleep -Seconds 5
-Rename-Item -Path ".\src" -NewName "PlasterTest" -Force -Confirm:$false
-$CurModuleVersion = (Import-PowerShellDataFile ".\PlasterTest\PlasterTest.psd1").ModuleVersion
-$PrevCommit = (git log --pretty=tformat:"%H")[1]
-git checkout -b buildtemp $PrevCommit --quiet
-$PrevModuleVersion = (Import-PowerShellDataFile ".\PlasterTest\PlasterTest.psd1").ModuleVersion
-git checkout master --quiet
-git branch -D buildtemp --quiet
-
-if ($CurModuleVersion -gt $PrevModuleVersion) {
-    Write-Output ("Module version increased from $PrevModuleVersion to $CurModuleVersion.`n" +
-                  "Publishing new version on PSGallery.")
-    Publish-Module -Path "$PSScriptRoot\..\PlasterTest" -NuGetApiKey $env:PSGALLERY_KEY
-}
-#>
-
-Start-Sleep -Seconds 5
-Rename-Item -Path ".\src" -NewName "PlasterTest" -Force -Confirm:$false
-$CurModuleVersion = (Import-PowerShellDataFile ".\PlasterTest\PlasterTest.psd1").ModuleVersion
-$Tag = git tag --points-at master
-
-if ($null -ne $Tag) {
-    if ($Tag -like "v$($CurModuleVersion.ToString)") {
+if ($env:APPVEYOR_REPO_TAG -eq "true") {
+    Rename-Item -Path ".\src" -NewName "PlasterTest" -Force -Confirm:$false
+    $CurModuleVersion = (Import-PowerShellDataFile ".\PlasterTest\PlasterTest.psd1").ModuleVersion
+    
+    if ($env:APPVEYOR_REPO_TAG_NAME -like "v$($CurModuleVersion.ToString)") {
         Write-Host "Current commit has been tagged with a new version. Pushing to PowerShell Gallery."
         #Publish-Module -Path "$PSScriptRoot\..\PlasterTest" -NuGetApiKey $env:PSGALLERY_KEY
     } else {
